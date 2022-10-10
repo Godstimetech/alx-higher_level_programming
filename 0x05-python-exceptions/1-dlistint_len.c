99161ef3a55ab1e2090cccbabb46876e7e3cbe76
GODSTIMETECH <excellentmarketbiz@gmail.com>
2022-10-10 13:53:44 -0700

    commit resolve

--- 0x05-python-exceptions/1-dlistint_len.c
+++ 0x05-python-exceptions/1-dlistint_len.c
@@ -7,13 +7,11 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t nodes = 0;
-
-	while (h)
-	{
-		h = h->next;
-		nodes += 1;
-	}
-
-	return (nodes);
+size_t nodes = 0;
+while (h)
+{
+h = h->next;
+nodes += 1;
+}
+return (nodes);
 }
